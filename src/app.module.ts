0afb44303830345ae8d2c9d115bfc7e76f091a59
dksalsgus <alsgus2ek@naver.com>
2021-10-20 19:09:19 +0900

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ChargerModule } from './charger/charger.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DBNAME,
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    UserModule,
+    ChargerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
